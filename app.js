c4144bde5401a466737b0c5fe355664ee582d37e
Max Korop <makskorop99@gmail.com>
2023-05-12 12:59:51 +0300

    added routing

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
-}).listen(3000);
+const mongoose = require('mongoose');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-console.log('Server running at http://127.0.0.1:3000/');
+const PORT = 3000
+
+mongoose.connect('mongodb://127.0.0.1:27017/project_bd', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(`MongoDB Connection Error: ${err}`));
+
+const server = http.createServer(function (request, response) {
+  response.setHeader('Content-Type', 'text/html');
+
+  const createPath = (page) => {return path.resolve(__dirname, 'views', `${page}.html`)};
+
+  let basePath = '';
+
+  switch (request.url){
+    case '/':
+      basePath = createPath('index');
+      break;
+    case '/dram':
+      basePath = createPath('dram');
+      break;
+    case '/druzhba':
+      basePath = createPath('druzhba');
+      break;
+    case '/fila':
+      basePath = createPath('fila');
+      break;
+    default:
+      basePath = createPath('index');
+      break
+  }
+
+  
+  fs.readFile(basePath, (err, data) => {
+    if(err){
+      console.log(err);
+      response.statusCode = 500;
+      response.end('Internal Server Error');
+    }
+    else{
+      response.setHeader('Content-Type', 'text/html');
+      response.statusCode = 200;
+      response.write(data);
+      response.end();
+    }
+  })
+})
+
+server.listen(PORT, '127.0.0.1', (error) => {error ? console.log(error) : console.log(`Server running at http://127.0.0.1:${PORT}/`);});
